9a727dac35300aa0477c353bf97cb8a2fa578152
Ualh <ualh@proton.me>
2024-10-28 08:50:07 +0100

    topic modelling attempt

--- src/setup.R
+++ src/setup.R
@@ -46,7 +46,7 @@ packages <- c(
   "jsonlite",
   
   # Text analysis
-  "quanteda", "quanteda.textstats", "quanteda.textplots", "tidytext", 
+  "quanteda", "quanteda.textstats", "quanteda.textplots", "quanteda.textmodels","tidytext", 
   "stringr", "tm", "wordcloud", "SnowballC", "stopwords", 
   "ggwordcloud", "broom", "igraph", "reshape2",
   
@@ -54,7 +54,9 @@ packages <- c(
   "caTools",
   
   # Topic modeling
-  "topicmodels"
+  "topicmodels", "seededlda", "topicmodels", "topicdoc",
+  
+  "lexicon"
 )
 
 
