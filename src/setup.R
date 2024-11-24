c4cf3f2b380a773686829814dd813b6e25f58bff
Ualh <ualh@proton.me>
2024-11-24 09:42:31 +0100

    debug setup.r, a comma was missing !

--- src/setup.R
+++ src/setup.R
@@ -28,7 +28,7 @@ packages <- c(
   "tidyverse", "lubridate", "dplyr", "tidyr",
   
   # Plotting
-  "patchwork", "maps", "scales", "ggmap", "ggplot2", "plotly", "ggrepel", "viridis", "visNetwork", "igraph"
+  "patchwork", "maps", "scales", "ggmap", "ggplot2", "plotly", "ggrepel", "viridis", "visNetwork", "igraph",
   
   # Reporting
   "knitr", "kableExtra", "rmarkdown", "flextable",
